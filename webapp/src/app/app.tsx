3be66b2119aa7f92af23f0ccfbaae76498c41a2b
med3633 <sallemomar@sfax.r-iset.tn>
2025-09-03 22:50:28 +0100

    create input and button on react

--- webapp/src/app/app.tsx
+++ webapp/src/app/app.tsx
@@ -1,20 +1,35 @@
-import { useCallback, useEffect, useState} from 'react';
+import { useCallback, useEffect, useState, useRef} from 'react';
 import axios from 'axios';
 import { Todo } from '@mp-hr/shared-types';
+import { get } from 'node_modules/axios/index.cjs';
 export function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const textInputRef = useRef<HTMLInputElement>(null);
   const getTodos = useCallback(async () => {
     const resp = await axios.get<Todo[]>('http://localhost:3000/api');
     setTodos(resp.data);
   }, []);
+  const handleAddTodo = useCallback(async () => {
+    if (textInputRef.current) {
+      await axios.post('http://localhost:3000/api', {
+        title: textInputRef.current.value
+      });
+      textInputRef.current.value = '';
+      getTodos();
+    }
+  }, [getTodos]);
 
   useEffect(() => {
     getTodos()
-  }, []);
+  }, [getTodos]);
 
   return (
     <div>
       {JSON.stringify(todos)}
+      <div>
+        <input ref={textInputRef} />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
     </div>
   );
 }
