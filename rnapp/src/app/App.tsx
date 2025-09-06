59c7f886755704e6ececf5d8d7c8790a2ee6d7de
med3633 <sallemomar@sfax.r-iset.tn>
2025-09-06 11:32:32 +0100

    run rn

---
+++ rnapp/src/app/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  return (
+    <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
+          >
+          </ScrollView>
+        </SafeAreaView>
+    </>
+  )
+}
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+});
+
+export default App;
