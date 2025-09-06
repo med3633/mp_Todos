ffff01884f0d32a7507b0ef0068868fada59454d
med3633 <sallemomar@sfax.r-iset.tn>
2025-09-06 11:51:53 +0100

    add hooks in da

--- data-access/src/lib/data-access.tsx
+++ data-access/src/lib/data-access.tsx
@@ -1,11 +1,46 @@
-import styles from './data-access.module.css';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Todo } from '../../../dist/shared-types/src';
 
-export function DataAccess() {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to DataAccess!</h1>
-    </div>
+export function useTodos() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const getTodos = useCallback(async () => {
+    const resp = await axios.get<{ todos: Todo[] }>('http://localhost:3000/api');
+    setTodos(resp.data.todos);
+  }, []);
+
+  const addTodo = useCallback(
+    async (title: string) => {
+      await axios.post('http://localhost:3000/api', {
+        title,
+      });
+      getTodos();
+    },
+    [getTodos]
   );
-}
 
-export default DataAccess;
+  const toggleTodo = useCallback(
+    async (id: number) => {
+      const currentTodo = todos.find((todo) => todo.id === id);
+      if (currentTodo) {
+        await axios.post(`http://localhost:3000/api/set-completed`, {
+          id,
+          completed: !currentTodo.completed,
+        });
+        getTodos();
+      }
+    },
+    [todos, getTodos]
+  );
+
+  useEffect(() => {
+    getTodos();
+  }, [getTodos]);
+
+  return {
+    todos,
+    addTodo,
+    toggleTodo,
+  };
+}
