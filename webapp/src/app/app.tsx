16265d334649b1e16305c4bb513e50b551292f07
med3633 <sallemomar@sfax.r-iset.tn>
2025-09-06 11:59:45 +0100

    import da in react

--- webapp/src/app/app.tsx
+++ webapp/src/app/app.tsx
@@ -1,38 +1,16 @@
-import { useCallback, useEffect, useState, useRef} from 'react';
-import axios from 'axios';
-import { Todo } from '../../../dist/shared-types/src';
-import { get } from 'node_modules/axios/index.cjs';
+import { useRef} from 'react';
+import { useTodos } from '@mp-hr/data-access';
+
 export function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const { todos, addTodo, toggleTodo } = useTodos();
   const textInputRef = useRef<HTMLInputElement>(null);
-  const getTodos = useCallback(async () => {
-    const resp = await axios.get<{ todos: Todo[] }>('http://localhost:3000/api');
-    setTodos(resp.data.todos);
-  }, []);
-  const handleAddTodo = useCallback(async () => {
-    if (textInputRef.current) {
-      await axios.post('http://localhost:3000/api', {
-        title: textInputRef.current.value
-      });
+
+  const handleAddTodo = () => {
+    if (textInputRef.current && textInputRef.current.value.trim()) {
+      addTodo(textInputRef.current.value);
       textInputRef.current.value = '';
-      getTodos();
     }
-  }, [getTodos]);
-
-  const onToggle = useCallback(async (id: number) => {
-    const currentTodo = todos.find((todo) => todo.id === id);
-    if (currentTodo) {
-      await axios.post(`http://localhost:3000/api/set-completed`, {
-        id: id,
-        completed: !currentTodo.completed
-      });
-      getTodos();
-    }
-  }, [todos, getTodos]);
-
-  useEffect(() => {
-    getTodos()
-  }, [getTodos]);
+  };
 
   return (
     <div>
@@ -40,7 +18,7 @@ export function App() {
         <div key={todo.id}>
           <input
             type="checkbox" checked={todo.completed}
-            onChange={()=> onToggle(todo.id)}
+            onChange={()=> toggleTodo(todo.id)}
           />
           {todo.title}
         </div>
